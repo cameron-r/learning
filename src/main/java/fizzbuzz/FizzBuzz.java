13ec097f6a7c49b99929b25c2208c84dc51bd3f8
Cameron <crowshanbin@gmail.com>
2018-07-29 11:54:07 -0400

    Tyler's first solution

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,13 @@ package fizzbuzz;
 
 public class FizzBuzz {
     public void fizzBuzz(int n) {
-
+        if (n%3==0 && n%5==0)
+            System.out.println("FizzBuzz");
+        else if (n%3==0)
+            System.out.println("Fizz");
+        else if (n%5==0)
+            System.out.println("Buzz");
+        else
+            System.out.println(n);
     }
 }
--- src/main/java/fizzbuzz/Main.java
+++ src/main/java/fizzbuzz/Main.java
@@ -3,6 +3,8 @@ package fizzbuzz;
 public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.fizzBuzz(100);
+
+        for (int i=1;i<=100;i++)
+            fizzBuzz.fizzBuzz(i);
     }
 }
